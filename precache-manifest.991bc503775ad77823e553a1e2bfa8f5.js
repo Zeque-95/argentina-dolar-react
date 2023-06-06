d9bf88d0975533d06af468b1df6f55f0643b6f67
Zeque-95 <galatsergio@gmail.com>
2023-06-06 01:30:03 -0300

    Updates

---
+++ precache-manifest.991bc503775ad77823e553a1e2bfa8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1d1604e855c43cf7348137959cdd07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "822537f9bdf1c65920a6",
+    "url": "/static/js/2.70563675.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.70563675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb77b781d624ef3f2ed",
+    "url": "/static/js/main.2bc3a504.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2bc3a504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e26f57bb4c011cbe27",
+    "url": "/static/js/runtime-main.7c7da18a.js"
+  }
+]);
