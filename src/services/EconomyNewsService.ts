88d765cd3e2cb0e8629077a97284f47a3b88d98f
Maximiliano Correa <jmaxicorrea@gmail.com>
2023-06-22 00:05:37 -0300

    Economy News Pagination!

--- src/services/EconomyNewsService.ts
+++ src/services/EconomyNewsService.ts
@@ -5,11 +5,23 @@ import { News } from "../domain/News";
 export class EconomyNewsService {
 
   static async getNews(): Promise<News[]> {
-     return Axios.get<News[]>("/news/economy/page/1")
+     return Axios.get<News[]>("/news/economy")
      .then((response) => {
        const { data } = response;
        return data;
      })
-    
   }
+
+
+  static async getNewsPage(
+    page : number
+  ): Promise<News[]> {
+    const url = `/news/economy/page/${page}`;
+    return Axios.get<News[]>(url)
+    .then((response) => {
+      const { data } = response;
+      return data;
+    })
+ }
+
 }
